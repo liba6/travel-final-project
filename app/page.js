e454eb38de63c12fa9a62aba7b5c52d07f1b9b19
liba6 <lginsburg@gmail.com>
2023-02-27 17:56:07 +0100

    Add one api

--- app/page.js
+++ app/page.js
@@ -1,91 +1,55 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+export default function getRestaurantsDataVienna () {
+  const axios = require("axios");
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+// api to get restaurants and their addresses in Vienna
+  const options = {
+    method: 'GET',
+    url: 'https://the-fork-the-spoon.p.rapidapi.com/restaurants/v2/list',
+    params: {queryPlaceValueCityId: '597321', pageSize: '10', pageNumber: '1'},
+    headers: {
+      'X-RapidAPI-Key': '2a9ed7a647mshfb4ab3e1468e295p10aac4jsna1239960128f',
+      'X-RapidAPI-Host': 'the-fork-the-spoon.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+    const dataArr = response.data.data
+    const restaurantNameAdd = dataArr.map((item)=> {return item.name + " " + item.address.street + " " + item.address.postalCode})
+   console.log('list', restaurantNameAdd);
+  }).catch(function (error) {
+    console.error(error);
+  });
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+  // api to get restaurants and their addresses in Jerusalem
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+  // api 
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+    // You should get your API key at https://opentripmap.io
+    const apiKey = "5ae2e3f221c38a28845f05b62586f509eadc68302230e84ebb9f73e0";
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    function apiGet(method, query) {
+      return new Promise(function(resolve, reject) {
+        var otmAPI =
+          "https://api.opentripmap.com/0.1/en/places/" +
+          method +
+          "?apikey=" +
+          apiKey;
+        if (query !== undefined) {
+          otmAPI += "&" + query;
+        }
+        fetch(otmAPI)
+          .then(response => response.json())
+          .then(data => resolve(data))
+          .catch(function(err) {
+            console.log("Fetch Error :-S", err);
+          });
+      });
+    }
+    
+return (
+  <div>
+  <h1>Trying to get apis asap!</h1>
+</div>
+)
 }
