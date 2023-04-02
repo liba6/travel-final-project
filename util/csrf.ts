fdeb22f182c87b850b7f0ae2c7ac2cf647a1eea4
liba6 <lginsburg@gmail.com>
2023-04-02 15:39:04 +0200

    Fix csrf util file

--- util/csrf.ts
+++ util/csrf.ts
@@ -3,16 +3,14 @@
 // const tokens = new Tokens();
 
 // // create a new secret/seed to store in the sessions table
-// export function createCsrfSecret() {
-//   return tokens.secretSync();
-// }
-
-// // use a secret/seed from a session to generate a csrf token
-// export function createTokenFromSecret(secret: string) {
-//   return tokens.create(secret);
-// }
-
-// // validate a csrf token against a secret/seed in the sessions table
-// export function validateTokenAgainstSecret(secret: string, token: string) {
-//   return tokens.verify(secret, token);
-// }
+export function createCsrfSecret() {
+  //   return tokens.secretSync();
+  // }
+  // // use a secret/seed from a session to generate a csrf token
+  // export function createTokenFromSecret(secret: string) {
+  //   return tokens.create(secret);
+  // }
+  // // validate a csrf token against a secret/seed in the sessions table
+  // export function validateTokenAgainstSecret(secret: string, token: string) {
+  //   return tokens.verify(secret, token);
+}
